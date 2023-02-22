14fca28de62529ea042db2c6525ad2aaacf6ca75
Harish_S_99 <s.harish2711@gmail.com>
2023-02-22 20:21:51 +0530

    second commit and added get request

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express();
 const PORT = process.env.SERVER_PORT || 3030;
 
 // your code
-
+app.get("/iotdblink", function (req, res) {
+  res.send(
+    "<p>name: " + req.query["name"] + "</p><p>age: " + req.query["age"] + "</p>"
+  );
+});
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
