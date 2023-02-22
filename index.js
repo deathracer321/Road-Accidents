7bfd4aeecc2e40fd861774c4a4c2663386f67b21
Harish_S_99 <s.harish2711@gmail.com>
2023-02-22 19:43:01 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.SERVER_PORT || 3030;
+
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
